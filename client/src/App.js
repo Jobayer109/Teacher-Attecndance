2541579c820e44b6b958404e2805db249c4fe200
user.email <jobayer.ahmed109@gmail.com>
2024-07-05 21:38:35 +0600

    Time count implemented

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import router from "./Routes/Routes";
 
 function App() {
   return (
-    <div className="bg-gradient-to-t from-blue-300 to-slate-200 shadow-lg min-h-screen">
+    <div className="bg-gradient-to-b from-blue-300  to-slate-200 shadow-lg min-h-screen">
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
--- client/src/Components/Hero/Hero.jsx
+++ client/src/Components/Hero/Hero.jsx
@@ -1,39 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoMdArrowRoundDown } from "react-icons/io";
 
 const Hero = () => {
+  let time = new Date().toLocaleTimeString();
+  const [currentTime, setCurrentTime] = useState(time);
+
+  const updateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setCurrentTime(time);
+  };
+
+  setInterval(updateTime, 1000);
   return (
     <section className="flex flex-col items-center justify-center relative">
       <div className="w-full mt-">
         <div className="text-center px-4">
           <h2 className="text-italic font-semibold text-gray-600 mb-2">
-            Hello Teacher,
+            Welcome to,
           </h2>
           <span className="text-4xl font-bold inline-block text-blue-700 mb-2">
-            Good Morning
+            Teacher's Attendance
           </span>
 
           <p className="text-gray-500 mb-4">
             Effortless Attendance Tracking for Teachers
           </p>
 
-          <button className=" text-gray-700 border border-red-900 px-4 py-2 mb-6 rounded-md transform transition duration-500 hover:scale-110  font-bold">
+          {/* <button className=" text-gray-700 border border-red-900 px-4 py-2 mb-6 rounded-md transform transition duration-500 hover:scale-110  font-bold">
             How It Works
-          </button>
+          </button> */}
+
+          <p className="text-4xl md:text-5xl lg:text-6xl font-bold text-gray-900 rounded-lg">
+            {currentTime}
+          </p>
         </div>
       </div>
 
       <div className="flex flex-col items-center justify-center  w-full py-8">
-        <h1 className="text-5xl font-bold mb-1 animate-bounce">
+        <h1 className="text-3xl font-bold mb-1 animate-bounce">
           <IoMdArrowRoundDown className="text-red-900" />
         </h1>
 
         <div className="w-full max-w-md">
-          <button className="fancy-button bg-blue-700 text-white px-6 py-3 rounded-lg shadow-lg transform transition duration-500 hover:scale-105 hover:bg-blue-900 mb-4 w-full font-medium">
+          <button className="fancy-button bg-green-700 text-white px-6 py-3 rounded-lg shadow-lg transform transition duration-500 hover:scale-105 hover:bg-green-900 mb-4 w-full font-medium">
             Generate QR Code
           </button>
 
-          <button className="fancy-button bg-green-700 text-white px-6 py-3 rounded-lg shadow-lg transform transition duration-500 hover:scale-105 hover:bg-green-900 w-full font-medium">
+          <button className="fancy-button bg-red-800 text-white px-6 py-3 rounded-lg shadow-lg transform transition duration-500 hover:scale-105 hover:bg-red-900 w-full font-medium">
             Scan QR Code
           </button>
         </div>
