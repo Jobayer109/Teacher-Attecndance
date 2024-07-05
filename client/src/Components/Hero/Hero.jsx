69f9f6e7dc81dae8d8a18010f0df904d925e63c3
user.email <jobayer.ahmed109@gmail.com>
2024-07-05 21:53:29 +0600

    Resolved some issues

--- client/src/Components/Hero/Hero.jsx
+++ client/src/Components/Hero/Hero.jsx
@@ -15,10 +15,8 @@ const Hero = () => {
     <section className="flex flex-col items-center justify-center relative">
       <div className="w-full mt-">
         <div className="text-center px-4">
-          <h2 className="text-italic font-semibold text-gray-600 mb-2">
-            Welcome to,
-          </h2>
-          <span className="text-4xl font-bold inline-block text-blue-700 mb-2">
+          <h2 className="text-2xl font-semibold text-gray-600">Welcome to,</h2>
+          <span className="text-4xl font-extrabold inline-block text-blue-700 mt-3 ">
             Teacher's Attendance
           </span>
 
@@ -30,23 +28,23 @@ const Hero = () => {
             How It Works
           </button> */}
 
-          <p className="text-4xl md:text-5xl lg:text-6xl font-bold text-gray-900 rounded-lg">
+          <p className="text-4xl md:text-4xl lg:text-5xl font-extrabold text-gray-900 rounded-lg">
             {currentTime}
           </p>
         </div>
       </div>
 
       <div className="flex flex-col items-center justify-center  w-full py-8">
-        <h1 className="text-3xl font-bold mb-1 animate-bounce">
+        <h1 className="text-3xl font-bold mt-3 animate-bounce">
           <IoMdArrowRoundDown className="text-red-900" />
         </h1>
 
         <div className="w-full max-w-md">
-          <button className="fancy-button bg-green-700 text-white px-6 py-3 rounded-lg shadow-lg transform transition duration-500 hover:scale-105 hover:bg-green-900 mb-4 w-full font-medium">
+          <button className="fancy-button bg-green-700 text-white px-6 py-4 rounded-lg shadow-lg transform transition duration-500 hover:scale-105 hover:bg-green-900 mb-4 w-full font-medium">
             Generate QR Code
           </button>
 
-          <button className="fancy-button bg-red-800 text-white px-6 py-3 rounded-lg shadow-lg transform transition duration-500 hover:scale-105 hover:bg-red-900 w-full font-medium">
+          <button className="fancy-button bg-red-800 text-white px-6 py-4 rounded-lg shadow-lg transform transition duration-500 hover:scale-105 hover:bg-red-900 w-full font-medium">
             Scan QR Code
           </button>
         </div>
